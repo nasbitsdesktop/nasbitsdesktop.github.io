77505bb84499d01ad5b21d8e8c61abf391b01899
nasbitsdesktop <jacobsnewfancyemail123a@gmail.com>
2024-08-05 07:01:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = 'd2adf6e34f3e2ba3f4113dfe9889757f'; // Replace with your OpenWeatherMap API key
-const city = 'Houston'; // Replace with your city name
+const city = 'Friendswood'; // Replace with your city name
 
 const weatherInfo = document.getElementById('weather-info');
 const weatherIcon = document.getElementById('weather-icon');
