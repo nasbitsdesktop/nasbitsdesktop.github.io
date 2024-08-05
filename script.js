438a51e7a602661928a8a043953aaeb74a311bef
nasbitsdesktop <jacobsnewfancyemail123a@gmail.com>
2024-08-05 07:30:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const apiKey = 'd2adf6e34f3e2ba3f4113dfe9889757f'; // Replace with your OpenWeatherMap API key
-const city = 'Houston'; // Replace with your city name
+const city = 'Friendswood'; // Replace with your city name
 
 const weatherInfo = document.getElementById('weather-info');
 const weatherIcon = document.getElementById('weather-icon');
 const temperature = document.getElementById('temperature');
 const description = document.getElementById('description');
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=impeerial`;
 
 async function fetchWeather() {
     try {
@@ -20,7 +20,7 @@ async function fetchWeather() {
             const temp = data.main.temp;
             const weatherDescription = weather.description;
 
-            temperature.textContent = `${Math.round(temp)}°C`;
+            temperature.textContent = `${Math.round(temp)}°F`;
             description.textContent = weatherDescription;
 
             updateWeatherIcon(weather.icon);
